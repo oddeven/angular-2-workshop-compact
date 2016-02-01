15998b14f684cc838cd2a155f872c2d95b3d48ab
Angular 2 Workshop <workshop-guest@oddeven.ch>
2016-02-01 21:35:54 +0100

    Added different types of template bindings

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,28 @@ import {Component} from 'angular2/core';
 @Component({
   selector: 'app',
   template: `
-    <p>{{message}}</p>
+    <input type="text"
+           [value]="value"
+           [attr.aria-invalid]="error"
+           [class.is-valid]="!error"
+           (input)="onInput($event.target.value)">
+    <p [textContent]="message"
+       [style.color]="error ? 'red' : 'green'"></p>
   `
 })
 export class App {
-  message : String;
-  constructor() {
-    this.message = 'Hello World!'
+  value: String = '';
+  message: String = 'All is fine...';
+  error: Boolean = false;
+
+  onInput(value) {
+    this.value = value;
+    if (this.value.length > 10) {
+      this.message = 'Value is too long';
+      this.error = true;
+    } else {
+      this.message = 'All is fine...';
+      this.error = false;
+    }
   }
 }
