b0e09e487e8af4f70a080cb72660b318e7b7904f
Angular 2 Workshop <workshop-guest@oddeven.ch>
2016-02-02 09:26:09 +0100

    Initial state for adding count down button

--- src/app.ts
+++ src/app.ts
@@ -2,39 +2,6 @@ import {Component} from 'angular2/core';
 
 @Component({
   selector: 'app',
-  template: `
-    <input type="text"
-           [value]="value"
-           [attr.aria-invalid]="error"
-           [class.is-valid]="!error"
-           (input)="onInput($event.target.value)">
-    <button (click)="reset()">Reset</button>
-    <p [textContent]="message"
-       [style.display]="error ? 'block' : 'none'"></p>
-  `
+  template: ``
 })
-export class App {
-  value : String;
-  message : String;
-  error : Boolean;
-
-  constructor() {
-    this.value = '';
-    this.error = false;
-  }
-
-  onInput(value) {
-    this.value = value;
-    if (this.value.length > 10) {
-      this.message = 'Value is too long';
-      this.error = true;
-    } else {
-      this.error = false;
-    }
-  }
-
-  reset() {
-    this.error = false;
-    this.value = '';
-  }
-}
+export class App {}
