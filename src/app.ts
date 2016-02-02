13818eb17b4e8fef233db70ce99b40d6fdd47478
Angular 2 Workshop <workshop-guest@oddeven.ch>
2016-02-02 16:39:55 +0100

    Initial state with separated collapsible-title component

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
 import {Component} from 'angular2/core';
-import {Collapsible} from './collapsible';
+import {Collapsible, CollapsibleTitle} from './collapsible';
 
 @Component({
   selector: 'app',
   template: `
     <collapsible (opened)="onOpened($event)"
                  (closed)="onClosed($event)">
-      <span collapsible-title>I'm a title</span>
+      <collapsible-title>I'm a title</collapsible-title>
       <p>I'm regular content that will be inserted into the collapsible body</p>
     </collapsible>
   `,
-  directives: [Collapsible]
+  directives: [Collapsible, CollapsibleTitle]
 })
 export class App {
   onOpened(collapsible) {
--- src/collapsible.ts
+++ src/collapsible.ts
@@ -1,10 +1,26 @@
 import {Component, Input, Output, EventEmitter} from 'angular2/core';
 
 @Component({
+  selector: 'collapsible-title',
+  template: `
+    <h3 [style.backgroundColor]="active ? 'yellow' : null">
+      <ng-content></ng-content>
+    </h3>
+  `
+})
+export class CollapsibleTitle {
+  active = false;
+
+  toggleActive() {
+    this.active = !this.active;
+  }
+}
+
+@Component({
   selector: 'collapsible',
   template: `
     <h3 (click)="toggle()">
-      <ng-content select="[collapsible-title]"></ng-content>
+      <ng-content select="collapsible-title"></ng-content>
     </h3>
     <div [style.display]="open ? 'block' : 'none'">
       <ng-content></ng-content>
