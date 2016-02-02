5f3de2b614c41f574b820b7f316173d69fd11d51
Angular 2 Workshop <workshop-guest@oddeven.ch>
2016-02-02 10:38:27 +0100

    Added input property for title and output property for closed event

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,22 @@ import {Collapsible} from './collapsible';
 @Component({
   selector: 'app',
   template: `
-    <collapsible [content]="content" (opened)="onOpened($event)"></collapsible>
+    <collapsible [title]="title"
+                 [content]="content"
+                 (opened)="onOpened($event)"
+                 (closed)="onClosed($event)"></collapsible>
   `,
   directives: [Collapsible]
 })
 export class App {
+  title = 'Simple collapsible';
   content = 'This is a simple collapsible content';
 
   onOpened(collapsible) {
     console.log('Collapsible was opened', collapsible);
+  }
+
+  onClosed(collapsible) {
+    console.log('Collapsible was closed', collapsible);
   }
 }
--- src/collapsible.ts
+++ src/collapsible.ts
@@ -3,15 +3,17 @@ import {Component, Input, Output, EventEmitter} from 'angular2/core';
 @Component({
   selector: 'collapsible',
   template: `
-    <h3 (click)="toggle()">Collapsible click to open</h3>
+    <h3 (click)="toggle()">{{title}}</h3>
     <div [style.display]="open ? 'block' : 'none'">
       {{content}}
     </div>
   `
 })
 export class Collapsible {
+  @Input() title;
   @Input() content;
   @Output() opened = new EventEmitter();
+  @Output() closed = new EventEmitter();
   open = false;
 
   toggle() {
@@ -19,6 +21,8 @@ export class Collapsible {
 
     if (this.open) {
       this.opened.next(this);
+    } else {
+      this.closed.next(this);
     }
   }
 }
