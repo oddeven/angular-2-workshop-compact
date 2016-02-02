725e13930bbfe4d5aa1206c3f5d9afd9fc7f1cb6
Angular 2 Workshop <workshop-guest@oddeven.ch>
2016-02-02 17:00:32 +0100

    Added view child query to toggle collapsible component through code

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import {Component} from 'angular2/core';
+import {Component, ViewChild} from 'angular2/core';
 import {Collapsible, CollapsibleTitle} from './collapsible';
 
 @Component({
@@ -9,15 +9,22 @@ import {Collapsible, CollapsibleTitle} from './collapsible';
       <collapsible-title>I'm a title</collapsible-title>
       <p>I'm regular content that will be inserted into the collapsible body</p>
     </collapsible>
+    <button (click)="toggleCollapsible()">Toggle collapsible</button>
   `,
   directives: [Collapsible, CollapsibleTitle]
 })
 export class App {
+  @ViewChild(Collapsible) collapsible;
+
   onOpened(collapsible) {
     console.log('Collapsible was opened', collapsible);
   }
 
   onClosed(collapsible) {
     console.log('Collapsible was closed', collapsible);
+  }
+
+  toggleCollapsible() {
+    this.collapsible.toggle();
   }
 }
